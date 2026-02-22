3c06ab244af557e07919ae56ed901448d000eaca
jungit66 <oizumiis2024@gmail.com>
2026-02-22 11:49:41 +0900

    chore: add postinstall prisma generate for Vercel builds

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
